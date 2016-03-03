d94ebc684417416e7405fb2ab9bfc2f6de95c4b6
Julien Corioland <julien.corioland@live.fr>
2016-03-03 21:39:55 +0100

    add image tagging

--- src/build-docker-images.sh
+++ src/build-docker-images.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 
 echo "Building MusicStore Catalog Docker Image"
-docker build -t jcorioland/musicstore-catalog:$1 -f Dockerfile.catalog .
+docker build -t jcorioland/musicstore-catalog -f Dockerfile.catalog .
+docker tag jcorioland/musicstore-catalog jcorioland/musicstore-catalog:$1
 
 echo "Building MusicStore Checkout Docker Image"
-docker build -t jcorioland/musicstore-checkout:$1 -f Dockerfile.checkout .
+docker build -t jcorioland/musicstore-checkout -f Dockerfile.checkout .
+docker tag jcorioland/musicstore-checkout jcorioland/musicstore-checkout:$1
 
 echo "Building MusicStore Front Docker Image"
-docker build -t jcorioland/musicstore-front:$1 -f Dockerfile.store .
+docker build -t jcorioland/musicstore-front -f Dockerfile.store .
+docker tag jcorioland/musicstore-front jcorioland/musicstore-front:$1
 
 echo "Done building images"
 echo "Pushing images..."
 
 docker login --username="$2" --password="$3" --email="$4"
-docker push jcorioland/musicstore-catalog:$1
-docker push jcorioland/musicstore-checkout:$1
-docker push jcorioland/musicstore-front:$1
+
+echo "Pushing Catalog image..."
+docker push jcorioland/musicstore-catalog
+
+echo "Pushing Checkout image..."
+docker push jcorioland/musicstore-checkout
+
+echo "Pushing Front image..."
+docker push jcorioland/musicstore-front
 
 docker logout
 
