18579653cc381616654d28272f73bfb8582172c9
Julien Corioland <julien.corioland@live.fr>
2016-03-03 21:22:39 +0100

    add images push to docker hub

--- src/build-docker-images.sh
+++ src/build-docker-images.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 echo "Building MusicStore Catalog Docker Image"
-docker build -t jcorioland/musicstore-catalog -f Dockerfile.catalog .
+docker build -t jcorioland/musicstore-catalog:$1 -f Dockerfile.catalog .
 
 echo "Building MusicStore Checkout Docker Image"
-docker build -t jcorioland/musicstore-checkout -f Dockerfile.checkout .
+docker build -t jcorioland/musicstore-checkout:$1 -f Dockerfile.checkout .
 
 echo "Building MusicStore Front Docker Image"
-docker build -t jcorioland/musicstore-front -f Dockerfile.store .
+docker build -t jcorioland/musicstore-front:$1 -f Dockerfile.store .
+
+echo "Done building images"
+echo "Pushing images..."
+
+docker login --username="$2" --password="$3" --email="$4"
+docker push jcorioland/musicstore-catalog:$1
+docker push jcorioland/musicstore-checkout:$1
+docker push jcorioland/musicstore-front:$1
+
+docker logout
 
 exit 0
