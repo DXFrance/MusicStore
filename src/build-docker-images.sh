ed2bb837a722024c9f0fbb847af23baf403ba364
Julien Corioland <julien.corioland@live.fr>
2016-03-03 20:55:51 +0100

    add bash script to create docker images in Visual Studio Team Services build

---
+++ src/build-docker-images.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Building MusicStore Catalog Docker Image"
+docker build -t jcorioland/musicstore-catalog -f Dockerfile.catalog
+
+echo "Building MusicStore Checkout Docker Image"
+docker build -t jcorioland/musicstore-checkout -f Dockerfile.checkout
+
+echo "Building MusicStore Front Docker Image"
+docker build -t jcorioland/musicstore-front -f Dockerfile.store
+
+exit 0
