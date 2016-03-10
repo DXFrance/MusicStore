50236fd3bc7c126df4f334e0299c3922d69794ae
Julien Corioland <julien.corioland@live.fr>
2016-03-10 15:05:50 +0100

    add a view component that display the version from project.json in the foot

---
+++ src/MusicStore/Components/VersionComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Mvc;
+using Microsoft.Extensions.PlatformAbstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore.Components
+{
+    [ViewComponent(Name = "Version")]
+    public class VersionComponent : ViewComponent
+    {
+        private string applicationVersion;
+
+        public VersionComponent(IApplicationEnvironment applicationEnvironment)
+        {
+            this.applicationVersion = applicationEnvironment.ApplicationVersion;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Default", this.applicationVersion);
+        }
+    }
+}
--- src/MusicStore/Startup.cs
+++ src/MusicStore/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using MusicStore.Components;
 using MusicStore.Models;
+using System.Reflection;
 
 namespace MusicStore
 {
