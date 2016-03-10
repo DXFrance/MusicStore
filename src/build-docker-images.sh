184dadb7be3329739a72abbc7ef7820a487a2767
Julien Corioland <julien.corioland@live.fr>
2016-03-10 15:20:43 +0100

    extract app version from project.json in docker images build script

--- src/build-docker-images.sh
+++ src/build-docker-images.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# get app version from project.json
+appVersion=$(cat MusicStore/project.json | jq -r '.version')
+echo "Building Docker images for MusicStore Version $appVersion"
+
 echo "Building MusicStore Catalog Docker Image"
 docker build -t jcorioland/musicstore-catalog -f Dockerfile.catalog .
-docker tag jcorioland/musicstore-catalog jcorioland/musicstore-catalog:$1
+docker tag jcorioland/musicstore-catalog jcorioland/musicstore-catalog:$appVersion
 
 echo "Building MusicStore Checkout Docker Image"
 docker build -t jcorioland/musicstore-checkout -f Dockerfile.checkout .
-docker tag jcorioland/musicstore-checkout jcorioland/musicstore-checkout:$1
+docker tag jcorioland/musicstore-checkout jcorioland/musicstore-checkout:$appVersion
 
 echo "Building MusicStore Front Docker Image"
 docker build -t jcorioland/musicstore-front -f Dockerfile.store .
-docker tag jcorioland/musicstore-front jcorioland/musicstore-front:$1
+docker tag jcorioland/musicstore-front jcorioland/musicstore-front:$appVersion
 
 echo "Done building images"
 echo "Pushing images..."
@@ -18,13 +22,13 @@ echo "Pushing images..."
 docker login --username="$2" --password="$3" --email="$4"
 
 echo "Pushing Catalog image..."
-docker push jcorioland/musicstore-catalog:$1
+docker push jcorioland/musicstore-catalog:$appVersion
 
 echo "Pushing Checkout image..."
-docker push jcorioland/musicstore-checkout:$1
+docker push jcorioland/musicstore-checkout:$appVersion
 
 echo "Pushing Front image..."
-docker push jcorioland/musicstore-front:$1
+docker push jcorioland/musicstore-front:$appVersion
 
 docker logout
 
